f313ba30741ce490c9af88abf1fd26ab2f75cc14
anloffredo <loffredo.ap@gmail.com>
2019-09-15 19:03:01 -0700

    Code Analysis Changes
    Code Analysis Changes

--- UserAuditableEntity.cs
+++ UserAuditableEntity.cs
@@ -9,7 +9,6 @@ namespace Penguin.Cms.Entities
     /// <summary>
     /// Base class for any entity that should have all changes logged along with identifying information of the person that made those changes
     /// </summary>
-    [Serializable]
     public abstract class UserAuditableEntity : AuditableEntity
     {
         #region Properties
@@ -17,45 +16,45 @@ namespace Penguin.Cms.Entities
         /// <summary>
         /// The Guid of the user that created the object
         /// </summary>
-        [DontAllow(DisplayContext.List | DisplayContext.Edit)]
-        [CustomRoute(DisplayContext.Edit, "Render", "UserRecord")]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        [CustomRoute(DisplayContexts.Edit, "Render", "UserRecord")]
         [Display(Name = "Created By")]
         public Guid Creator { get; set; }
 
         /// <summary>
         /// The Guid of the user that deleted the object
         /// </summary>
-        [DontAllow(DisplayContext.Any)]
-        [CustomRoute(DisplayContext.Edit, "Render", "UserRecord")]
+        [DontAllow(DisplayContexts.Any)]
+        [CustomRoute(DisplayContexts.Edit, "Render", "UserRecord")]
         [Display(Name = "Deleted By")]
         public Guid Deleter { get; set; }
 
         /// <summary>
         /// The IP address of the user that created the object
         /// </summary>
-        [DontAllow(DisplayContext.List | DisplayContext.Edit)]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
         [Display(Name = "Creating IP")]
         public string IPCreated { get; set; }
 
         /// <summary>
         /// The IP Address of the user that deleted the object
         /// </summary>
-        [DontAllow(DisplayContext.List | DisplayContext.Edit)]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
         [Display(Name = "Deleting IP")]
         public string IPDeleted { get; set; }
 
         /// <summary>
         /// The IP Address of the last user to make changes to the object
         /// </summary>
-        [DontAllow(DisplayContext.List | DisplayContext.Edit)]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
         [Display(Name = "Last Modifying IP")]
         public string IPModified { get; set; }
 
         /// <summary>
         /// The Guid of the last user to make changes to the object
         /// </summary>
-        [DontAllow(DisplayContext.List | DisplayContext.Edit)]
-        [CustomRoute(DisplayContext.Edit, "Render", "UserRecord")]
+        [DontAllow(DisplayContexts.List | DisplayContexts.Edit)]
+        [CustomRoute(DisplayContexts.Edit, "Render", "UserRecord")]
         [Display(Name = "Last Modified By")]
         public Guid LastModifier { get; set; }
 
